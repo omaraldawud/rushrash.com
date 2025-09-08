ca5716fa9b9d909c69b95ee37a12a095f0936157
Omar Aldawud <omaldawud@noctrl.edu>
2025-09-07 19:50:48 -0500

    feat: updated vercel.json 2nd try

--- api/chat.js
+++ api/chat.js
@@ -5,7 +5,7 @@ import offersList from "../data/offersList.js";
 
 export default async function handler(req, res) {
   console.log("API route hit", req.method);
-  res.status(200).json({ message: "API is working" });
+  // res.status(200).json({ message: "API is working" });     use for test route
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
